a98dd0526a131f5dcb95b0a08df6a2a32144dc4e
Victor G <victorgordian103@gmail.com>
2018-12-05 13:01:08 -0800

    hm

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -35,9 +35,9 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars, id } = movie;
+  const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card" key={id}>
+    <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
